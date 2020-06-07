b6b7b27704bcad605fded42453459e8ab50142d8
Nicolás Castro <nicolas.castro@bci.cl>
2020-06-07 18:20:06 -0400

    Add basic Airflow implementation

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# : "${POSTGRES_HOST:="airflow_postgres"}"
+# : "${POSTGRES_PORT:="5432"}"
+# : "${POSTGRES_USER:="airflow"}"
+# : "${POSTGRES_PASSWORD:="airflow"}"
+# : "${POSTGRES_DB:="airflow"}"
+
+airflow initdb
+airflow webserver
